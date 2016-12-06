750b11422d8df159a8d71329e92abdd1a64e2b92
Marten Gajda <marten@dmfs.org>
2016-12-06 12:15:16 +0100

    Add initial implementation of an API call to load an event by its UID

---
+++ eventsdiscovery/src/main/java/com/schedjoules/client/eventsdiscovery/http/SingleEventResponseHandler.java
@@ -0,0 +1,81 @@
+/*
+ * Copyright 2016 SchedJoules
+ *
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.schedjoules.client.eventsdiscovery.http;
+
+import com.schedjoules.client.eventsdiscovery.Envelope;
+import com.schedjoules.client.eventsdiscovery.Event;
+import com.schedjoules.client.eventsdiscovery.json.JsonEvent;
+import org.dmfs.httpessentials.client.HttpResponse;
+import org.dmfs.httpessentials.client.HttpResponseHandler;
+import org.dmfs.httpessentials.converters.PlainStringHeaderConverter;
+import org.dmfs.httpessentials.exceptions.ProtocolError;
+import org.dmfs.httpessentials.exceptions.ProtocolException;
+import org.dmfs.httpessentials.headers.BasicSingletonHeaderType;
+import org.dmfs.httpessentials.headers.SingletonHeaderType;
+import org.dmfs.httpessentials.responsehandlers.StringResponseHandler;
+import org.json.JSONObject;
+
+import java.io.IOException;
+
+
+/**
+ * An {@link HttpResponseHandler} that parses a JSON response containing a single {@link Event} object.
+ *
+ * @author Marten Gajda
+ */
+public final class SingleEventResponseHandler implements HttpResponseHandler<Envelope<Event>>
+{
+    private final static SingletonHeaderType<String> ETAG_HEADER = new BasicSingletonHeaderType<>("etag", PlainStringHeaderConverter.INSTANCE);
+
+
+    @Override
+    public Envelope<Event> handleResponse(final HttpResponse response) throws IOException, ProtocolError, ProtocolException
+    {
+        final JSONObject jsonObject = new JSONObject(new StringResponseHandler().handleResponse(response));
+
+        return new Envelope<Event>()
+        {
+            @Override
+            public String etag()
+            {
+                return response.headers().header(ETAG_HEADER).value();
+            }
+
+
+            @Override
+            public String uid()
+            {
+                return payload().uid();
+            }
+
+
+            @Override
+            public boolean hasPayload()
+            {
+                return true;
+            }
+
+
+            @Override
+            public Event payload()
+            {
+                return new JsonEvent(jsonObject);
+            }
+        };
+    }
+}
---
+++ eventsdiscovery/src/main/java/com/schedjoules/client/eventsdiscovery/queries/EventByUid.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright 2016 SchedJoules
+ *
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.schedjoules.client.eventsdiscovery.queries;
+
+import com.schedjoules.client.Api;
+import com.schedjoules.client.ApiQuery;
+import com.schedjoules.client.eventsdiscovery.Envelope;
+import com.schedjoules.client.eventsdiscovery.Event;
+import com.schedjoules.client.eventsdiscovery.http.GetRequest;
+import com.schedjoules.client.eventsdiscovery.http.SingleEventResponseHandler;
+import org.dmfs.httpessentials.exceptions.ProtocolError;
+import org.dmfs.httpessentials.exceptions.ProtocolException;
+import org.dmfs.httpessentials.types.Token;
+
+import java.io.IOException;
+import java.net.URI;
+
+
+/**
+ * An {@link ApiQuery} to retrieve a single event by its UID.
+ *
+ * @author Marten Gajda
+ */
+public final class EventByUid implements ApiQuery<Envelope<Event>>
+{
+    private final static String QUERY_PATH = "/events/";
+
+    private final Token mUid;
+
+
+    private EventByUid(Token uid)
+    {
+        mUid = uid;
+    }
+
+
+    @Override
+    public Envelope<Event> queryResult(Api api) throws IOException, ProtocolError, ProtocolException
+    {
+        return api.queryResult(URI.create(QUERY_PATH).resolve(mUid.toString()), new GetRequest<>(new SingleEventResponseHandler()));
+    }
+}
